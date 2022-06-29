4ad98ddc798faf98ed6550653272051600493bce
Leonidas <leonidas.ramirez.q@gmail.com>
2022-06-28 21:50:24 -0500

    added alert component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -33,6 +33,10 @@ export class RegisterComponent {
     Validators.maxLength(12)
   ]);
 
+  showAlert = false;
+  alertMsg = "Please wait! Your account is being created";
+  alertColor = "blue";
+
   registerForm = new FormGroup({
     name: this.name,
     email: this.email,
@@ -43,6 +47,8 @@ export class RegisterComponent {
   });
 
   register() {
-    console.log("Register called")
+    this.showAlert = true;
+    this.alertMsg = "Please wait! Your account is being created";
+    this.alertColor = "blue";
   }
 }
