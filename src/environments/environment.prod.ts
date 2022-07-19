645061653ed0cb27792ccebdbb756d184bea6918
Leonidas <leonidas.ramirez.q@gmail.com>
2022-07-19 07:38:05 -0500

    user registration with fire auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDL4IX5GJ85LTufJG_b0uqmuhxoObd7LHw",
+    authDomain: "clips-14718.firebaseapp.com",
+    projectId: "clips-14718",
+    storageBucket: "clips-14718.appspot.com",
+    appId: "1:139249409961:web:3677e38ce2c676e1e3aeb8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyDL4IX5GJ85LTufJG_b0uqmuhxoObd7LHw",
+      authDomain: "clips-14718.firebaseapp.com",
+      projectId: "clips-14718",
+      storageBucket: "clips-14718.appspot.com",
+      appId: "1:139249409961:web:3677e38ce2c676e1e3aeb8"
+  }
 };
 
 /*
