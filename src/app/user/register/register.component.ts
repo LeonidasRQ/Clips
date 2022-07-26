4edae6a8ada356080449229a2d12b7206d03a76c
Leonidas <leonidas.ramirez.q@gmail.com>
2022-07-26 13:25:52 -0500

    implemented password validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
 
 @Component({
   selector: 'app-register',
@@ -51,7 +52,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber,
-  });
+  },  [RegisterValidators.match("password", "confirm_password")]);
 
   async register() {
     this.showAlert = true;
