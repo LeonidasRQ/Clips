d50839cbf6f3117989c4467394d6408199838b9e
Leonidas <leonidas.ramirez.q@gmail.com>
2022-08-08 07:43:29 -0500

    added alert component on file upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import {v4 as uuid  } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -8,8 +11,26 @@ import { Component, OnInit } from '@angular/core';
 export class UploadComponent implements OnInit {
   isDragover = false;
   file: File | null = null;
+  nextStep = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Your clip is being uploaded.';
+  inSubmission = false;
 
-  constructor() { }
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable:true
+  })
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+  constructor(
+    private storage: AngularFireStorage
+    ) { }
 
   ngOnInit(): void {
   }
@@ -23,6 +44,21 @@ export class UploadComponent implements OnInit {
       return
     }
 
-    console.log(this.file);
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    );
+    this.nextStep = true;
+  }
+
+  uploadFile() {
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Your clip is being uploaded.';
+    this.inSubmission = true;
+
+    const clipFileName = uuid();
+    const clipPath= `clips/${clipFileName}.mp4`;
+
+    this.storage.upload(clipPath, this.file);
   }
 }
