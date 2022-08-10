25a1e573657ade7c57cc96b6360192c7e746c16a
Leonidas <leonidas.ramirez.q@gmail.com>
2022-08-10 07:37:47 -0500

    sorting clips with BehaviorSubjects

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, Params} from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
 import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage',
@@ -10,23 +11,27 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
-  videoOrder = '1';
-  clips: IClip[] = [];
-  activeClip: IClip | null = null;
+  videoOrder = '1'
+  clips: IClip[] = []
+  activeClip: IClip | null = null
+  sort$: BehaviorSubject<string>
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private clipService: ClipService,
     private modal: ModalService
-    ) { }
+    ) {
+      this.sort$ = new BehaviorSubject(this.videoOrder)
+     }
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: Params) => {
-      this.videoOrder = params.sort === '2' ? params.sort : '1';
+      this.videoOrder = params.sort === '2' ? params.sort : '1'
+      this.sort$.next(this.videoOrder)
     })
 
-    this.clipService.getUserClips().subscribe(docs => {
+    this.clipService.getUserClips(this.sort$).subscribe(docs => {
       this.clips = []
 
       docs.forEach(doc => {
