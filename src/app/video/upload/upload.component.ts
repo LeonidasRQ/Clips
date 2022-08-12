7814edff873631f1caeaae962746a925b2444d66
Leonidas <leonidas.ramirez.q@gmail.com>
2022-08-12 17:27:14 -0500

    Initializing FFmpeg

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,6 +7,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
 import { Router } from '@angular/router';
+import { FfmpegService } from 'src/app/services/ffmpeg.service';
 
 @Component({
   selector: 'app-upload',
@@ -14,17 +15,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnDestroy {
-  isDragover = false;
-  file: File | null = null;
-  nextStep = false;
-  showAlert = false;
-  alertColor = 'blue';
-  alertMsg = 'Please wait! Your clip is being uploaded.';
-  inSubmission = false;
-  percentage = 0;
-  showPercentage = false;
-  user: firebase.User | null = null;
-  task?: AngularFireUploadTask;
+  isDragover = false
+  file: File | null = null
+  nextStep = false
+  showAlert = false
+  alertColor = 'blue'
+  alertMsg = 'Please wait! Your clip is being uploaded.'
+  inSubmission = false
+  percentage = 0
+  showPercentage = false
+  user: firebase.User | null = null
+  task?: AngularFireUploadTask
 
   title = new FormControl('', {
     validators: [
@@ -41,17 +42,19 @@ export class UploadComponent implements OnDestroy {
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
     private clipsService: ClipService,
-    private router: Router
+    private router: Router,
+    public ffmpegService: FfmpegService
     ) { 
-      auth.user.subscribe(user => this.user = user);
+      auth.user.subscribe(user => this.user = user)
+      this.ffmpegService.init()
     }
 
   ngOnDestroy(): void {
-    this.task?.cancel();
+    this.task?.cancel()
   }
 
   storeFile($event: Event) {
-    this.isDragover = false;
+    this.isDragover = false
 
     this.file = ($event as DragEvent).dataTransfer ?  
     ($event as DragEvent).dataTransfer?.files.item(0) ?? null :
@@ -64,23 +67,23 @@ export class UploadComponent implements OnDestroy {
     this.title.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
     );
-    this.nextStep = true;
+    this.nextStep = true
   }
 
   uploadFile() {
-    this.uploadForm.disable();
+    this.uploadForm.disable()
 
-    this.showAlert = true;
-    this.alertColor = 'blue';
-    this.alertMsg = 'Please wait! Your clip is being uploaded.';
-    this.inSubmission = true;
-    this.showPercentage = true;
+    this.showAlert = true
+    this.alertColor = 'blue'
+    this.alertMsg = 'Please wait! Your clip is being uploaded.'
+    this.inSubmission = true
+    this.showPercentage = true
 
     const clipFileName = uuid();
-    const clipPath= `clips/${clipFileName}.mp4`;
+    const clipPath= `clips/${clipFileName}.mp4`
 
-    this.task = this.storage.upload(clipPath, this.file);
-    const clipRef = this.storage.ref(clipPath);
+    this.task = this.storage.upload(clipPath, this.file)
+    const clipRef = this.storage.ref(clipPath)
 
     this.task.percentageChanges().subscribe(progress => {
       this.percentage = progress as number / 100
@@ -100,13 +103,13 @@ export class UploadComponent implements OnDestroy {
           timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }
 
-        const clipDocRef = await this.clipsService.createClip(clip);
+        const clipDocRef = await this.clipsService.createClip(clip)
 
-        console.log(clip);
+        console.log(clip)
 
-        this.alertColor = 'green';
-        this.alertMsg = 'Success! Your clip is now ready to be shared with the world.';
-        this.showPercentage = false;
+        this.alertColor = 'green'
+        this.alertMsg = 'Success! Your clip is now ready to be shared with the world.'
+        this.showPercentage = false
 
         setTimeout(() => {
           this.router.navigate([
@@ -115,13 +118,13 @@ export class UploadComponent implements OnDestroy {
         }, 1000)
       },
       error: (error) => {
-        this.uploadForm.enable();
+        this.uploadForm.enable()
 
-        this.alertColor = 'red';
-        this.alertMsg = 'Upload failed! Please try again later.';
-        this.inSubmission = true;
-        this.showPercentage = false;
-        console.error(error);
+        this.alertColor = 'red'
+        this.alertMsg = 'Upload failed! Please try again later.'
+        this.inSubmission = true
+        this.showPercentage = false
+        console.error(error)
       }
     })
   }
